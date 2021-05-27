79a90053ab1ece1f8f7c8337d1f35b0dce5c4dbc
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-05-27 18:14:20 -0300

    Project created gio

---
+++ loja-dh-giovanni/src/main/java/com/grupo3/lojadh/LojadhApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.lojadh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadhApplication.class, args);
+	}
+
+}
---
+++ loja-dh-giovanni/src/test/java/com/grupo3/lojadh/LojadhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.lojadh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojadhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
